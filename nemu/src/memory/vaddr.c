273be16999d91bffe32ab4b5ce60e99363f28696
tracer-ics2025 <tracer@njuics.org>
2026-03-20 22:04:44 +0800

    >  compile NEMU 231220000 张三 Linux zyb-AI 6.8.0-106-generic #106~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar  6 08:44:59 UTC  x86_64 x86_64 x86_64 GNU/Linux  22:04:44 up  7:35,  1 user,  load average: 2.74, 2.16, 1.95

--- nemu/src/memory/vaddr.c
+++ nemu/src/memory/vaddr.c
@@ -15,19 +15,15 @@
 
 #include <isa.h>
 #include <memory/paddr.h>
-#include <memory/vaddr.h>
 
 word_t vaddr_ifetch(vaddr_t addr, int len) {
-  paddr_t paddr = isa_mmu_translate(addr, len, MEM_TYPE_IFETCH);
-  return paddr_read(paddr, len);
+  return paddr_read(addr, len);
 }
 
 word_t vaddr_read(vaddr_t addr, int len) {
-  paddr_t paddr = isa_mmu_translate(addr, len, MEM_TYPE_READ);
-  return paddr_read(paddr, len);
+  return paddr_read(addr, len);
 }
 
 void vaddr_write(vaddr_t addr, int len, word_t data) {
-  paddr_t paddr = isa_mmu_translate(addr, len, MEM_TYPE_WRITE);
-  paddr_write(paddr, len, data);
+  paddr_write(addr, len, data);
 }
