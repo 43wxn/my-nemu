a358a1d474679f897c88d971bcd526c6cde9e702
tracer-ics2025 <tracer@njuics.org>
2026-03-30 21:49:43 +0800

    >  compile NEMU 231220000 张三 Linux zyb-AI 6.8.0-106-generic #106~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar  6 08:44:59 UTC  x86_64 x86_64 x86_64 GNU/Linux  21:49:43 up  6:11,  1 user,  load average: 2.50, 2.06, 1.76

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -9,7 +9,7 @@ void do_syscall(Context *c) {
   a[2] = c->GPR3;  // arg1
   a[3] = c->GPR4;  // arg2
 
-  // 调试时可打开；正式跑测试建议关掉，避免输出太多干扰
+  // 调试时打开，平时建议注释掉
   // printf("do_syscall: no=%ld arg0=%ld arg1=%ld arg2=%ld\n",
   //     a[0], a[1], a[2], a[3]);
 
@@ -28,23 +28,18 @@ void do_syscall(Context *c) {
       const char *buf = (const char *)a[2];
       size_t len = a[3];
 
-      if (fd == 1 || fd == 2) {  // stdout or stderr
+      if (fd == 1 || fd == 2) {
         for (size_t i = 0; i < len; i++) {
           putch(buf[i]);
         }
-        // write 成功时返回实际写入的字节数
         c->GPRx = len;
       } else {
-        // 当前 Nanos-lite 只支持标准输出/错误
         c->GPRx = -1;
       }
       break;
     }
 
     case SYS_brk:
-      // 在 Nanos-lite 当前阶段，一般不真正维护复杂堆空间，
-      // 只需告诉用户层“设置成功”即可。
-      // 用户层 _sbrk() 自己记录 program break。
       c->GPRx = 0;
       break;
 
--- navy-apps/libs/libos/src/syscall.h
+++ navy-apps/libs/libos/src/syscall.h
@@ -2,7 +2,7 @@
 #define __SYSCALL_H__
 
 enum {
-  SYS_exit,
+  SYS_exit = 0,
   SYS_yield,
   SYS_open,
   SYS_read,
