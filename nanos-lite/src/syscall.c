40906b5f33de1f76d7c33d0dbd9b9123a8311cad
tracer-ics2025 <tracer@njuics.org>
2026-03-30 17:49:22 +0800

    >  compile NEMU 231220000 张三 Linux zyb-AI 6.8.0-106-generic #106~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar  6 08:44:59 UTC  x86_64 x86_64 x86_64 GNU/Linux  17:49:22 up  2:11,  1 user,  load average: 1.77, 1.70, 1.72

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -9,10 +9,13 @@ void do_syscall(Context *c) {
   a[2] = c->GPR3;
   a[3] = c->GPR4;
 
+  printf("do_syscall: no=%ld arg0=%ld arg1=%ld arg2=%ld\n",
+      a[0], a[1], a[2], a[3]);
+
   switch (a[0]) {
     case SYS_yield:
-      yield();
       c->GPRx = 0;
+      yield();
       break;
 
     case SYS_exit:
