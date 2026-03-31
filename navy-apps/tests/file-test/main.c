d9d036c5e84d8c40309e341eff7beb49b52ef4cb
tracer-ics2025 <tracer@njuics.org>
2026-03-31 14:36:27 +0800

    >  compile NEMU 231220000 张三 Linux zyb-AI 6.8.0-106-generic #106~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar  6 08:44:59 UTC  x86_64 x86_64 x86_64 GNU/Linux  14:36:27 up  2:15,  1 user,  load average: 1.07, 1.26, 1.41

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -1,40 +1,63 @@
 #include <stdio.h>
 #include <assert.h>
+#include <unistd.h>
+
+static void mark(const char *s) {
+  write(1, s, 2);   // 例如 "A\n"
+}
 
 int main() {
+  mark("A\n");
   FILE *fp = fopen("/share/files/num", "r+");
+  mark(fp ? "B\n" : "b\n");
   assert(fp);
 
   fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
+  mark("C\n");
   assert(size == 5000);
 
   fseek(fp, 500 * 5, SEEK_SET);
+  mark("D\n");
+
   int i, n;
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
+    if (n != i + 1) {
+      mark("E\n");
+    }
     assert(n == i + 1);
   }
 
   fseek(fp, 0, SEEK_SET);
+  mark("F\n");
+
   for (i = 0; i < 500; i ++) {
     fprintf(fp, "%4d\n", i + 1 + 1000);
   }
 
+  mark("G\n");
+
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
+    if (n != i + 1) {
+      mark("H\n");
+    }
     assert(n == i + 1);
   }
 
   fseek(fp, 0, SEEK_SET);
+  mark("I\n");
+
   for (i = 0; i < 500; i ++) {
     fscanf(fp, "%d", &n);
+    if (n != i + 1 + 1000) {
+      mark("J\n");
+    }
     assert(n == i + 1 + 1000);
   }
 
   fclose(fp);
-
-  printf("PASS!!!\n");
-
+  mark("K\n");
   return 0;
 }
